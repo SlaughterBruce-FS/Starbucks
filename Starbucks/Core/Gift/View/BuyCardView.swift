eb2ca3e037050b5b7fd28ceefac4a98a407a5062
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2023-07-02 18:43:47 -0400

    Updated gift view

---
+++ Starbucks/Core/Gift/View/BuyCardView.swift
@@ -0,0 +1,18 @@
+//
+//  BuyCardView.swift
+//  Starbucks
+//
+//  Created by J C on 7/2/23.
+//
+
+import SwiftUI
+
+struct BuyCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuyCardView()
+}
---
+++ Starbucks/Core/Gift/View/GiftcardScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  GiftcardScrollView.swift
+//  Starbucks
+//
+//  Created by J C on 7/2/23.
+//
+
+import SwiftUI
+
+struct GiftcardScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GiftcardScrollView()
+}
