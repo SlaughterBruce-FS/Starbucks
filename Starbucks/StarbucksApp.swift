72240e83fa383caed0c4cb42b3a09cc8973475c8
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2023-07-02 12:49:33 -0400

    Initial Commit

---
+++ Starbucks/StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by J C on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
