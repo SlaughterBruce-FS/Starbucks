e8b68b06d093c770633b098713c1a74244b96c38
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2023-07-03 14:33:58 -0400

    addes smaller card view then added pages to the tab view

---
+++ Starbucks/Core/Gift/View/GifcardScrollSmall.swift
@@ -0,0 +1,18 @@
+//
+//  GifcardScrollSmall.swift
+//  Starbucks
+//
+//  Created by J C on 7/3/23.
+//
+
+import SwiftUI
+
+struct GifcardScrollSmall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GifcardScrollSmall()
+}
